e04f9d538f9cf7316a393a7fafe8e8e6d0e02d0a
Hallocoos <henricuswouter@gmail.com>
2019-05-21 14:20:53 +0200

    memcpy, strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,29 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-vos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 10:14:50 by hde-vos           #+#    #+#             */
-/*   Updated: 2019/05/21 14:17:22 by hde-vos          ###   ########.fr       */
+/*   Created: 2019/05/21 14:09:55 by hde-vos           #+#    #+#             */
+/*   Updated: 2019/05/21 14:15:44 by hde-vos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-char	*ft_strncpy(char *dest, char *src, int n)
+char	*ft_strdup(const char *s1)
 {
-	int i;
+	int size;
 
-	i = 0;
-	if (n > 0)
-	{
-		while (i++ < n)
-			dest[i] = src[i];
-		dest[i] = '\0';
-	}
-	else
-		dest = "";
-	return (dest);
+	size = strlen(s1) + 1;
+	void *new = malloc (size);
+	if (new == NULL)
+		return (NULL)
+	return (*s1);
 }
