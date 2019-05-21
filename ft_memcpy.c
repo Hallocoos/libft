3639ad68d3d7eeffe9c5437c6a63864b5e4ca6b2
Hallocoos <henricuswouter@gmail.com>
2019-05-21 14:26:31 +0200

    toupper, tolower fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,19 @@
 /*   By: hde-vos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:20:00 by hde-vos           #+#    #+#             */
-/*   Updated: 2019/05/21 14:20:01 by hde-vos          ###   ########.fr       */
+/*   Updated: 2019/05/21 14:24:48 by hde-vos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	int i;
 
+	i = 0;
+	if (n-- > 0)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	return (dst);
+}
